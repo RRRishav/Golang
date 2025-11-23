3662c7c08776f41b4f055bbd1817382e23ed80dc
RRRishav <rishav6787@gmail.com>
2025-11-23 16:22:48 +0530

    adding files

--- 6_ifelse/main.go
+++ 6_ifelse/main.go
@@ -4,12 +4,29 @@ import "fmt"
 
 func main() {
 
-	age := 18
+	// age := 18
 
-	if age >= 18 {
+	// if age >= 18 {
+	// 	fmt.Println("person is adult")
+	// } else {
+    //     fmt.Println("NOT ADULT")
+	// }
+
+
+	// var role = "admin"
+    // var hasPermitted = true
+
+	// if role == "admin" && hasPermitted {
+	// 	fmt.Println("yes")
+	// }
+
+
+	if age := 15; age >= 18 {
 		fmt.Println("person is adult")
 	} else {
-        fmt.Println("NOT ADULT")
+		fmt.Println("NOT ADULT")
 	}
+	//go doesnot have ternary use if and else only
 
+	
 }
