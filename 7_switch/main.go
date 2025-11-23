b2198c315058d67d66f331163ce08447ea48af23
RRRishav <rishav6787@gmail.com>
2025-11-23 16:27:50 +0530

    add additionals

--- 7_switch/main.go
+++ 7_switch/main.go
@@ -3,16 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	i := 1
+// 	i := 1
 
-	switch i {
-	case 1:
-		fmt.Println("one")
+// 	switch i {
+// 	case 1:
+// 		fmt.Println("one")
 	
-    case 2:
-	    fmt.Println("two")
+//     case 2:
+// 	    fmt.Println("two")
 
-	default:
-		fmt.Println("no case")
+// 	default:
+// 		fmt.Println("no case")
+// }
+
+    //type switch
+
+	whoAmi := func (i interface{})  {
+
+		switch t := i.(type){
+		case int:
+			fmt.Println("its integer")
+		case string:
+			fmt.Println("its a string")
+
+		default:{
+			fmt.Println("other",t)
+		}
+		}
+		
+	}
+
+
+	whoAmi("gola")
 }
-}
+
+
+
